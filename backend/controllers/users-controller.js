722c44ba3c59b70edf0dc5de3e17dc77ff275e46
yjyjyjy <prowessyang@gmail.com>
2020-11-05 22:35:01 -0800

    added uploader backend

--- backend/controllers/users-controller.js
+++ backend/controllers/users-controller.js
@@ -1,5 +1,4 @@
 const HttpError = require("../models/http-error");
-const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
 const User = require("../models/user");
 const dlog = require("../util/log");
--- backend/routes/users-route.js
+++ backend/routes/users-route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 const { getUsers, signup, login } = require("../controllers/users-controller");
+const fileUploader = require("../middleware/file-upload");
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ router.get("/", getUsers);
 
 router.post(
   "/signup",
+  fileUploader.single("image"), // expecting a key named image
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
